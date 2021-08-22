731028b3175f175c0718970095aaa2dd0eef16fe
Pearlyna <pearlyna_lim@hotmail.com>
2021-08-22 11:54:45 +0930

    Basic register html

---
+++ app/frontend/login_page/index.js
@@ -0,0 +1,25 @@
+//react example from website
+//ONLY FOR REFERENCE
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked comment number ';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#testReact');
+ReactDOM.render(e(LikeButton), domContainer);
