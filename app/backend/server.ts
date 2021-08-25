a966d0a52e7653883ea92d4bee264b3d57e413d7
Duy Tran <Duy.Tran@tristar.com.au>
2021-08-26 02:04:09 +0930

    Added ts files for server.js and hedera.js

---
+++ app/backend/server.ts
@@ -0,0 +1,36 @@
+const express = require('express')
+const { uploadToBlockChain } = require('./hederaAPI/hedera')
+const cors = require('cors')
+const app = express()
+const multer = require('multer');
+const upload = multer();
+const { returnToUser } = require('../database/index')
+const { getFileContent } = require('./hederaAPI/hedera')
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const port:number = 8080;
+//takes pdf payload from server and gets encrypted version into server; 
+app.post('/upload', upload.single('pdf'), function (req, res) {
+  uploadToBlockChain(req.file)
+  res.send("Finshed")
+})
+// returns the testUserObject
+app.get('/getUser', async (req, res) => {
+
+  let user = await returnToUser('TestUser')
+  res.send(user);
+})
+app.get('/getFile/:TxHash', async (req, res) => {
+
+  let content = await getFileContent(req.params.TxHash)
+  res.send(content);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
