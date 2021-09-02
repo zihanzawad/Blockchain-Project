de1a790c58ac87f9aceb6974a5ab5d16971a5791
magicusingjunta <50309940+magicusingjunta@users.noreply.github.com>
2021-09-02 17:16:29 +0930

    added encryption and recomposition function, edited test file for my local machine

--- app/backend/Scripts/Transformer.py
+++ app/backend/Scripts/Transformer.py
@@ -1,10 +1,12 @@
-from pdf2image import convert_from_path
+from pdf2image import convert_from_path, convert_from_bytes
 from PIL import Image
 from os import path, makedirs
 import numpy as np
+from hashlib import sha256
 
 class Transformer():
-
+    
+    #read pdf from file path and convert to jpegs
     def pdf_to_image(inputPath:str, outputPath:str):
         if not path.exists(outputPath):
             makedirs(outputPath)
@@ -14,7 +16,13 @@ class Transformer():
             fileName = outputPath + 'output' + str(pageNum)
             page.save(fileName, "JPEG")
         return pdfAsImages
+    
+    #read pdf from byte input and convert to jpegs
+    def bytes_to_images(bytes:bytes):
+        pdfAsImages = convert_from_bytes(bytes)
+        return pdfAsImages
 
+    #convert PIL arrays to numpy arrays
     def PIL_to_Numpy(input:list):
         pagesAsNumpy = []
         for page in input:
@@ -22,9 +30,31 @@ class Transformer():
             pagesAsNumpy.append(pageAsNumpy)
         return pagesAsNumpy
     
+    #separate a page into 18 separate chunks
     def PDF_to_Numpy(imagesAsNumpy: list, chunks: int=18) -> list:
         chunkedImages = []
         for image in imagesAsNumpy:
             chunked_image = np.array_split(image, chunks)
             chunkedImages.append(chunked_image)
         return chunkedImages
+
+    #return SHA256 hash of input
+    def encrypt_data(data):
+        hash = sha256(data)
+        return hash.hexdigest()
+
+    #convert chunked numpy representation into array of SHA256 hashes
+    def encrypt_document(input:list):
+        encryptedPages = []
+        for page in input:
+            for chunk in page:
+                encryptedPages.append(Transformer.encrypt_data(chunk))
+        return encryptedPages
+
+    #converts bytes to array of SHA256 hash strings
+    def bytes_to_hash_array(bytes:bytes):
+        images = Transformer.bytes_to_images(bytes)
+        pilArray = Transformer.PIL_to_Numpy(images)
+        npArray = Transformer.PDF_to_Numpy(pilArray)
+        hashArray = Transformer.encrypt_document(npArray)
+        return hashArray
--- app/backend/Scripts/pdf_to_image.py
+++ app/backend/Scripts/pdf_to_image.py
@@ -1,13 +1,14 @@
-from transformer import transformer as tf
+from Transformer import Transformer as tf
 from PIL import Image
+import base64
 
 # TEST FILE
 
 inf = "/home/huy/Desktop/isml_stuff/cover7-converted.pdf"
 outf = "/home/huy/Documents"
 
-a = tf.pdf_to_image(inf, outf)
-b = tf.PIL_to_Numpy(a)
-c = tf.PDF_to_Numpy(b)
-im = Image.fromarray(b[0])
-im.save('1.png')
+with open("/home/huy/Desktop/isml_stuff/cover7-converted.pdf", 'rb') as pdf_file:
+    images = pdf_file.read()
+
+a = tf.bytes_to_hash_array(images)
+print(a)
