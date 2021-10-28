536244da6f2f062c165d58f486c81093ebe7d027
jennyytran <50324711+jennyytran@users.noreply.github.com>
2021-10-29 01:18:33 +1030

    uncomment existing function

--- app/backend/public/js/upload.js
+++ app/backend/public/js/upload.js
@@ -1,40 +1,40 @@
-// document.querySelectorAll(".dropInput").forEach((inputElement) => {
-//     // Finds the closest element with the class dropZone
-//     const dropZoneElement = inputElement.closest(".dropZone");
+document.querySelectorAll(".dropInput").forEach((inputElement) => {
+    // Finds the closest element with the class dropZone
+    const dropZoneElement = inputElement.closest(".dropZone");
 
-//     /*inputElement.addEventListener("change", (e) => {
-//         updateThumbnail(dropZoneElement, inputElement.files[0]);
-//     });*/
+    /*inputElement.addEventListener("change", (e) => {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+    });*/
 
-//     // Add class dropOver when dragging a file over the dropZone div
-//     dropZoneElement.addEventListener("dragover", e => {
-//         e.preventDefault();
-//         dropZoneElement.classList.add("dropOver");
-//     });
+    // Add class dropOver when dragging a file over the dropZone div
+    dropZoneElement.addEventListener("dragover", e => {
+        e.preventDefault();
+        dropZoneElement.classList.add("dropOver");
+    });
 
-//     ["dragleave", "dragend"].forEach(type => {
-//         // Removing dropOver class after leaving dropzone or have dropped the file
-//         dropZoneElement.addEventListener(type, e => {
-//             dropZoneElement.classList.remove("dropOver");
-//         });
-//     });
+    ["dragleave", "dragend"].forEach(type => {
+        // Removing dropOver class after leaving dropzone or have dropped the file
+        dropZoneElement.addEventListener(type, e => {
+            dropZoneElement.classList.remove("dropOver");
+        });
+    });
 
-//     dropZoneElement.addEventListener("drop", e => {
-//         e.preventDefault();
-//         // If there is a file
-//         if (e.dataTransfer.getData("text")) {
-//             let input = document.querySelector("input.btn");
-//             const DT = new DataTransfer();
-//             let file = (new File(['foo'] , e.dataTransfer.getData("text")));
-//             file.name = e.dataTransfer.getData("text");
-//             DT.items.add(file);
-//             input.files = DT.files;
-//         }
+    dropZoneElement.addEventListener("drop", e => {
+        e.preventDefault();
+        // If there is a file
+        if (e.dataTransfer.getData("text")) {
+            let input = document.querySelector("input.btn");
+            const DT = new DataTransfer();
+            let file = (new File(['foo'] , e.dataTransfer.getData("text")));
+            file.name = e.dataTransfer.getData("text");
+            DT.items.add(file);
+            input.files = DT.files;
+        }
 
-//         dropZoneElement.classList.remove("dropOver");
-//     });
+        dropZoneElement.classList.remove("dropOver");
+    });
 
-// });
+});
 
 Dropzone.options.dropzoneFrom = {
     autoProcessQueue: false, 
