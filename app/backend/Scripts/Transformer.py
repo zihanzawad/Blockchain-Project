f1506f3c62680de66ea514a032909bfae82d61d0
magicusingjunta <50309940+magicusingjunta@users.noreply.github.com>
2021-10-28 22:10:08 +1030

    added transformer stuff

--- app/backend/Scripts/Transformer.py
+++ app/backend/Scripts/Transformer.py
@@ -2,33 +2,16 @@ from pdf2image import convert_from_path, convert_from_bytes
 from os import path, makedirs
 from hashlib import sha256
 import numpy as np
-import sys
-import base64
-from PIL import Image
 
 
 class Transformer():
 
-    #read pdf from file path and convert to jpegs
-    def save_pdf_as_image(inputPath:str, outputPath:str):
-        if not path.exists(outputPath):
-            makedirs(outputPath)
-
-        pdfAsImages = convert_from_path(inputPath)
-        for pageNum, page in enumerate(pdfAsImages):
-            fileName = outputPath + 'output' + str(pageNum)
-            page.save(fileName, "JPEG")
-        return pdfAsImages
-
     def pdf_as_images(inputPath: str):
-        #pdfAsImages = convert_from_path(bytes,poppler_path=r'C:\Program Files\poppler-21.10.0\Library\bin')
         pdfAsImages = convert_from_path(inputPath)
         return pdfAsImages
 
     #read pdf from byte input and convert to jpegs
-    def bytes_to_images(inputpath:str):
-        bytes = inputpath.encode()
-        #pdfAsImages = convert_from_bytes(bytes,poppler_path=r'C:\Program Files\poppler-21.10.0\Library\bin')
+    def bytes_to_images(bytes:bytes):
         pdfAsImages = convert_from_bytes(bytes)
         return pdfAsImages
 
@@ -41,11 +24,15 @@ class Transformer():
         return pagesAsNumpy
 
     #separate a page into 18 separate chunks
-    def PDF_to_Numpy(imagesAsNumpy: list, chunks: int=18) -> list:
+    def numpy_splitter(imagesAsNumpy: list, chunks: int=18) -> list:
         chunkedImages = []
         for image in imagesAsNumpy:
-            chunked_image = np.array_split(image, chunks)
-            chunkedImages.append(chunked_image)
+            current_image = []
+            chunked_image_row = np.array_split(image, chunks)
+            for row in chunked_image_row:
+                chunked_row = np.array_split(row, chunks, axis=1)
+                current_image.append(chunked_row)
+            chunkedImages.append(current_image)
         return chunkedImages
 
     #return SHA256 hash of input
@@ -58,8 +45,11 @@ class Transformer():
         encryptedPages = []
         for page in input:
             currentPage = []
-            for chunk in page:
-                currentPage.append(Transformer.encrypt_data(chunk))
+            for chunk_row in page:
+                row = []
+                for chunk in chunk_row:
+                    row.append(Transformer.encrypt_data(np.ascontiguousarray(chunk)))
+                currentPage.append(row)
             encryptedPages.append(currentPage)
         return encryptedPages
 
@@ -68,13 +58,13 @@ class Transformer():
         #images = Transformer.bytes_to_images(inputPath)
         images = Transformer.pdf_as_images(inputPath)
         pilArray = Transformer.PIL_to_Numpy(images)
-        npArray = Transformer.PDF_to_Numpy(pilArray)
+        npArray = Transformer.numpy_splitter(pilArray)
         hashArray = Transformer.encrypt_document(npArray)
         return hashArray
 
     def images_to_hash_array(images:list):
         pilArray = Transformer.PIL_to_Numpy(images)
-        npArray = Transformer.PDF_to_Numpy(pilArray)
+        npArray = Transformer.numpy_splitter(pilArray)
         hashArray = Transformer.encrypt_document(npArray)
         return hashArray
 
@@ -83,9 +73,10 @@ class Transformer():
         tamperedRegions = []
         if len(original) == len(toVerify):
             for pageNum in range(len(original)):
-                for chunkNum in range(len(original[pageNum])):
-                    if original[pageNum][chunkNum] != toVerify[pageNum][chunkNum]:
-                        tamperedRegions.append([pageNum, chunkNum])
+                for rowNum in range(len(original[pageNum])):
+                    for chunkNum in range(len(original[pageNum][rowNum])):
+                        if original[pageNum][rowNum][chunkNum] != toVerify[pageNum][rowNum][chunkNum]:
+                            tamperedRegions.append([pageNum, rowNum, chunkNum])
             if bool(tamperedRegions):
                 return tamperedRegions
         else:
@@ -93,17 +84,20 @@ class Transformer():
         return 0
 
     # Highlights tampered areas
-    def visualise_tamper(pagesAsNumpy:list, tamperedRegions:list, chunks: int = 18):
+    def visualise_tamper(pagesAsNumpy:list, tamperedRegions:list, v_chunks: int = 18, h_chunks: int = 18):
 
         pages = np.array(pagesAsNumpy,dtype=float)/255
 
         for region in tamperedRegions:
             page = region[0]
-            chunk = region[1]
-            lower = round(np.shape(pages[page])[0]*chunk/chunks)
-            upper = round(np.shape(pages[page])[0]*(chunk+1)/chunks)
-            pages[page,lower:upper,:,1] *= 0.4
-            pages[page,lower:upper,:,2] *= 0.4
+            v_chunk = region[1]
+            h_chunk = region[2]
+            v_lower = round(np.shape(pages[page])[0]*v_chunk/v_chunks)
+            v_upper = round(np.shape(pages[page])[0]*(v_chunk+1)/v_chunks)
+            h_lower = round(np.shape(pages[page])[1]*h_chunk/h_chunks)
+            h_upper = round(np.shape(pages[page])[1]*(h_chunk+1)/h_chunks)
+            pages[page,v_lower:v_upper, h_lower:h_upper, 1] *= 0.4
+            pages[page,v_lower:v_upper, h_lower:h_upper, 2] *= 0.4
 
         scrollFile = np.array()
         for i in range(len(pages)):
