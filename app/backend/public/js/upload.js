420bba7af5288767089be2cd8a240081e9c1367f
jennyytran <50324711+jennyytran@users.noreply.github.com>
2021-10-29 15:38:12 +1030

    removing excess comments

--- app/backend/public/js/upload.js
+++ app/backend/public/js/upload.js
@@ -1,5 +1,5 @@
     // Finds the closest element with the class dropZone
-    dropZoneElement = document.getElementById("thedropzone");
+    var dropZoneElement = document.getElementById("thedropzone");
 
     /*inputElement.addEventListener("change", (e) => {
         updateThumbnail(dropZoneElement, inputElement.files[0]);
@@ -20,62 +20,10 @@
 
     dropZoneElement.addEventListener("drop", e => {
         e.preventDefault();
-        // If there is a file
-        // if (e.dataTransfer.getData("pdf")) {
-        //     let input = document.querySelector("input.btn");
-        //     const DT = new DataTransfer();
-        //     let file = (new File(['foo'] , e.dataTransfer.getData("pdf")));
-        //     file.name = e.dataTransfer.getData("pdf");
-        //     DT.items.add(file);
-        //     input.files = DT.files;
-        // }
         uploadPDF.files = e.dataTransfer.files;
-
-        // If you want to use some of the dropped files
-        const dT = new DataTransfer();
-        dT.items.add(e.dataTransfer.files[0]);
-        //dT.items.add(e.dataTransfer.files[3]);
-        uploadPDF.files = dT.files;
 
         dropZoneElement.classList.remove("dropOver");
     });
-
-
-Dropzone.options.dropzoneFrom = {
-    autoProcessQueue: false, 
-    paramName: "pdf",
-    acceptedFiles:".pdf",
-    uploadMultiple: false,
-    init: function(){
-        var myDropzone = this;
-        this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
-            // Make sure that the form isn't actually being sent.
-            e.preventDefault();
-            e.stopPropagation();
-            myDropzone.processQueue();
-        });
-        
-    // var submitButton = document.querySelector('#submit-all');
-    //  myDropzone = this;
-    //  submitButton.addEventListener("click", function(){
-    //  myDropzone.processQueue();
-    //  });
-     this.on("complete", function(){
-      if(this.getQueuedFiles().length == 0 && this.getUploadingFiles().length == 0)
-      {
-       var _this = this;
-       _this.removeAllFiles();
-      }
-      testing();
-     });
-    },
-   };
-
-function testing()
- {
-   console.log("testing");
- }
-
 
 
 /*function updateThumbnail(dropZoneElement, file) {
